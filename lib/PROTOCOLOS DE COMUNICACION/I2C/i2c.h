22ec4d437bfc41b8487fbc56223634510f350d08
joacomiras <joaquinmiras8@gmail.com>
2025-10-17 10:19:27 -0300

    Corrección

--- lib/PROTOCOLOS DE COMUNICACION/I2C/i2c.h
+++ lib/PROTOCOLOS DE COMUNICACION/I2C/i2c.h
@@ -3,12 +3,12 @@
 
 #include "stm32f103xb.h"
 
-void i2c_init(I2C_TypeDef* i2c_inst);
-void i2c_start(I2C_TypeDef* i2c_inst);
-void i2c_stop(I2C_TypeDef* i2c_inst);
-void i2c_send_direccion(I2C_TypeDef* i2c_inst, unsigned char direccion);
-void i2c_send_byte(I2C_TypeDef* i2c_inst, unsigned char dato);
-unsigned char i2c_recibir_byte_ack(I2C_TypeDef* i2c_inst);
-unsigned char i2c_recibir_byte_nack(I2C_TypeDef* i2c_inst);
+void i2c_init(I2C_TypeDef* );
+void i2c_start(I2C_TypeDef* );
+void i2c_stop(I2C_TypeDef* );
+void i2c_send_direccion(I2C_TypeDef* , unsigned char);
+void i2c_send_byte(I2C_TypeDef* , unsigned char);
+unsigned char i2c_recibir_byte_ack(I2C_TypeDef*);
+unsigned char i2c_recibir_byte_nack(I2C_TypeDef*);
 
 #endif
