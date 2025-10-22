4d5dde9390a2e3a2eec41c7e1b86ac5942fcfd07
joacomiras <89646468+joacomiras@users.noreply.github.com>
2025-10-22 16:12:51 -0300

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
 
-
 #define RXD2 16 // Conectar al PA9 (TX) del STM32
 #define TXD2 17 // Conectar al PA10 (RX) del STM32
 
 void setup() {
-    // Serial del monitor serie
+
     Serial.begin(115200);
+    delay(2000); 
     Serial.println("ESP32 (Esclavo UART) listo.");
 
-    // Serial2 porque el 1 es el monitor serie
+
     Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+
 }
 
 void loop() {
     if (Serial2.available() > 0) {
-        
-        // Leer el carácter recibido
         char c = Serial2.read();
-
         Serial.print("Carácter recibido: ");
-        Serial.write(c); // Usar .write() es mejor para bytes
+        Serial.write(c);
+        Serial.println();
     }
 }
