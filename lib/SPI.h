a2f9c46b8d5fb3608918fe3484ca3346fd56b6af
joacomiras <89646468+joacomiras@users.noreply.github.com>
2025-11-13 15:31:09 -0300

    Add 8-bit and 16-bit SPI functions

--- lib/SPI.h
+++ lib/SPI.h
@@ -6,6 +6,9 @@
 void spi_init(SPI_TypeDef* spi_inst);
 void spi_ss(SPI_TypeDef* spi_inst);
 void spi_ds(SPI_TypeDef* spi_inst); //deseleccionar slave
-unsigned char spi_rw_byte(SPI_TypeDef* spi_inst, unsigned char dato);
+void spi_8bit(SPI_TypeDef* spi_inst);
+unsigned char spi_8bit_rw(SPI_TypeDef* spi_inst, unsigned char dato);
+void spi_16bit(SPI_TypeDef* spi_inst);
+uint16_t spi_16bit_rw(SPI_TypeDef* spi_inst, uint16_t dato);
 
 #endif
